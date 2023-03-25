c839bc43d6c679bd9687e66e23df2f3a9da5921c
Helen L <htslin103@gmail.com>
2023-03-24 21:22:52 -0600

    Made some app settings changes and db connection change

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace MvcMovie
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddDbContext<MvcMovieContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection") ?? throw new InvalidOperationException("Connection string 'defaultConnection' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
